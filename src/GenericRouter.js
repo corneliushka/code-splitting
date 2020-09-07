0b7404cb016b1e29965c0470535f841794277a0a
corneliushka <cga@synapsis-group.com>
2020-09-07 11:20:24 +0200

    Route-based code splitting

--- src/GenericRouter.js
+++ src/GenericRouter.js
@@ -1,57 +1,65 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import {
-    BrowserRouter as Router,
+    BrowserRouter,
     Switch,
+    Redirect,
     Route,
     Link
 } from "react-router-dom";
 
 import Home from "./chunked-components/Home"
-import Chart from "./chunked-components/Chart"
-import Table from "./chunked-components/Table"
-import RichText from "./chunked-components/RichText"
+//import Chart from "./chunked-components/Chart"
+//import Table from "./chunked-components/Table"
+//import RichText from "./chunked-components/RichText"
 
+
+const Table = lazy( () => import('./chunked-components/Table'));
+const Chart = lazy( () => import ('./chunked-components/Chart'));
+const RichText = lazy( () => import('./chunked-components/RichText'))
 
 export default function App() {
-return (
-    <Router>
-    <div>
-        <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/table">Table</Link>
-            </li>
-            <li>
-                <Link to="/chart">Chart</Link>
-            </li>
-            <li>
-                <Link to="/richtext">RichText</Link>
-            </li>
-        </ul>
-        </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-            <Route path="/">
-                <Home />
-            </Route>
-            <Route path="/table">
-                <Table />
-            </Route>
-            <Route path="/chart">
-                <Chart />
-            </Route>
-            <Route path="/richtext">
-                <RichText />
-            </Route>
 
-        </Switch>
-    </div>
-    </Router>
-);
+    /* const loadTable = () => {
+        import('./chunked-components/Table')
+            .then( ({ Table }) => {
+                // use module Table
+
+            })
+    } */
+
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/table">Table</Link>
+                        </li>
+                        <li>
+                            <Link to="/chart">Chart</Link>
+                        </li>
+                        <li>
+                            <Link to="/richtext">RichText</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/home" component={Home} />
+                    <Route path="/table" component={Table} />
+                    <Route exact path="/chart" component={Chart} />
+                    <Route exact path="/richtext" component={RichText} />
+                    <Redirect from="/" to="/home" />
+
+                </Switch>
+            </Suspense>
+        </BrowserRouter>
+    );
 }
 
--- src/chunked-components/Table.js
+++ src/chunked-components/Table.js
@@ -1,10 +1,22 @@
 import React from "react"
+import MaterialTable from 'material-table'
+
 
 
 export default function Table() { // material-table chunk
     return (
         <>
             <h2>Table</h2>
+            <MaterialTable
+                columns={[
+                    { title: 'Adı', field: 'name' },
+                    { title: 'Soyadı', field: 'surname' },
+                    { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+                    { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' } }
+                ]}
+                data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
+                title="Demo Title"
+            />
         </>
     )
 }
